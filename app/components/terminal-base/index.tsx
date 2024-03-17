32f10888166c51b4537ce70ee409b892b2c3f553
Kevin Faveri <kevin@faveri.dev>
2024-03-17 17:04:16 -0300

    add scroll indicator

--- app/components/terminal-base/index.tsx
+++ app/components/terminal-base/index.tsx
@@ -1,8 +1,15 @@
-import { useRef, ReactNode } from "react";
+import { useRef, ReactNode, useEffect, useState, useCallback } from "react";
 import TerminalOutput from "./linetypes/terminal-output";
 import TerminalInput from "./linetypes/terminal-input";
 import { useTerminalState } from "./useTerminalState";
-import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import {
+  FaChevronDown,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import useInterval from "~/hooks/useInterval";
 
 export interface Props {
   name?: string;
@@ -14,7 +21,6 @@ export interface Props {
   currentLineInput?: ReactNode;
 }
 
-// TODO: Add a way to indicate there's scroll still (useful for mobile)
 // TODO: Add shortcut if you press enter or tap screen it just speed up all animations and render all steps
 // TODO: If you click close, show matrix (easter egg)
 const Terminal = ({
@@ -25,13 +31,36 @@ const Terminal = ({
   greenBtnCallback,
 }: Props) => {
   const { state } = useTerminalState();
+  const [hasMoreToScroll, setHasMoreToScroll] = useState(false);
 
-  const scrollIntoViewRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const checkForScroll = useCallback(() => {
+    const current = scrollRef.current;
+    if (current) {
+      const { scrollTop, scrollHeight, clientHeight } = current;
+      setHasMoreToScroll(scrollTop + clientHeight < scrollHeight);
+    }
+  }, []);
+
+  useInterval(checkForScroll, 1000);
+
+  useEffect(() => {
+    const current = scrollRef.current;
+    if (current) {
+      current.addEventListener("scroll", checkForScroll);
+    }
+    return () => {
+      if (current) {
+        current.removeEventListener("scroll", checkForScroll);
+      }
+    };
+  }, [checkForScroll]);
 
   return (
     <div className="react-terminal-wrapper h-full overflow-hidden">
       <div className="flex justify-between items-center px-4 py-0 rounded-tl-3xl mb-2 rounded-tr-3xl bg-[#363d4a] sticky top-0 z-10">
-        <div className="react-terminal-window-buttons space-x-2">
+        <div className="react-terminal-window-buttons space-x-2 flex-1">
           <button
             className="clickable red-btn p-0 h-[10px] w-[10px] md:h-[15px] md:w-[15px]"
             disabled={!redBtnCallback}
@@ -48,7 +77,7 @@ const Terminal = ({
             onClick={greenBtnCallback}
           />
         </div>
-        <div className="flex justify-between items-center space-x-2">
+        <div className="flex justify-between items-center space-x-2 flex-1">
           <div className="text-primary-green font-bold text-xs sm:text-sm md:text-lg">
             {name}
           </div>
@@ -99,17 +128,27 @@ const Terminal = ({
           </div>
         </div>
       </div>
-      <div className="react-terminal h-full px-4 overflow-auto box-border py-5">
+      <div
+        className="react-terminal h-full px-4 overflow-auto box-border py-5"
+        ref={scrollRef}
+        onScroll={checkForScroll}
+      >
         {state?.terminalLineData}
         <div
           className="react-terminal-line react-terminal-input react-terminal-active-input flex flex-col md:flex-row"
           key="terminal-line-prompt"
         >
-          <div className="react-terminal-prompt text-primary-green mr-1">{prompt || ">"}</div>
+          <div className="react-terminal-prompt text-primary-green mr-1">
+            {prompt || ">"}
+          </div>
           <div>{state?.currentLine}</div>
         </div>
-        <div ref={scrollIntoViewRef}></div>
       </div>
+      {hasMoreToScroll && (
+        <div className="absolute bottom-1 flex justify-center w-full text-primary-green animate-bounce">
+          <FaChevronDown size={36} />
+        </div>
+      )}
     </div>
   );
 };
