389b420de991a7d54d2de053522988429d56a82a
Kevin Faveri <kevin@faveri.dev>
2024-03-17 20:35:27 -0300

    og fix

--- app/root.tsx
+++ app/root.tsx
@@ -37,12 +37,16 @@ export default function App() {
           rel="stylesheet"
         />
         <link rel="icon" href={iconSrc} />
-        <meta property="og:image" content="https://kevin.faveri.dev/og" />
+        <meta property="og:image" content="/og" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:url" content="https://kevin.faveri.dev" />
         <meta property="og:title" content="Kevin Faveri's Portfolio" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/og" />
+        <meta name="twitter:url" content="https://kevin.faveri.dev" />
+        <meta name="twitter:title" content="Kevin Faveri's Portfolio" />
         <meta
           property="og:description"
           content="Kevin Faveri | Portfolio | Web3, Ordinals, Solana, Open Source and more..."
